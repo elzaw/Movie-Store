3a75bb6f754a6fae71cdebae949bdd47e6dd6e39
elzaw <hoodaelzaw81@gamil.com>
2024-03-05 02:06:13 +0200

    spinner

--- src/components/MovieDetails/index.jsx
+++ src/components/MovieDetails/index.jsx
@@ -26,7 +26,11 @@ const MovieDetails = () => {
   }
 
   if (!movieDetails) {
-    return <div>Loading...</div>;
+    return (
+      <div class=" flex justify-center items-center">
+        <div class="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+      </div>
+    );
   }
 
   const handleWatchClick = () => {
--- src/components/MoviesList/index.jsx
+++ src/components/MoviesList/index.jsx
@@ -14,6 +14,7 @@ import { fetchMovies } from "../../store/slices/movies";
 const Movies = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [searchQuery, setSearchQuery] = useState("");
+  const [loading, setLoading] = useState(true); // State for loading
   const popularMovies = useSelector((state) => state.movies.movies);
   const error = useSelector((state) => state.movies.error);
   const totalPages = useSelector((state) => state.movies.totalPages);
@@ -21,7 +22,10 @@ const Movies = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchMovies({ currentPage, searchQuery }));
+    setLoading(true); // Set loading to true when fetching starts
+    dispatch(fetchMovies({ currentPage, searchQuery }))
+      .then(() => setLoading(false)) // Set loading to false when fetching completes
+      .catch(() => setLoading(false)); // Set loading to false if there's an error
   }, [currentPage, searchQuery]);
 
   const handleNextPage = () => {
@@ -59,85 +63,94 @@ const Movies = () => {
           onChange={handleSearchInputChange}
         ></TEInput>
       </div>
-      <div className="flex flex-wrap -mx-4">
-        {error && <div>Error: {error}</div>}
-        {popularMovies.map((movie) => (
-          <div
-            key={movie.id}
-            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-8"
-          >
-            <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-              <TERipple>
-                <div className="relative overflow-hidden bg-cover bg-no-repeat">
-                  <img
-                    className="rounded-t-lg"
-                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                    alt={movie.title}
-                  />
-                  <button
-                    className="absolute top-4 right-4 z-50"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      e.stopPropagation();
-                      favorites.some((favMovie) => favMovie.id === movie.id)
-                        ? handleRemoveFromFavorites(movie.id)
-                        : handleAddToFavorites(movie);
-                    }}
-                  >
-                    {favorites.some((favMovie) => favMovie.id === movie.id) ? (
-                      <FaHeart size={30} style={{ color: "red" }} />
-                    ) : (
-                      <CiHeart size={30} style={{ color: "white" }} />
-                    )}
-                  </button>
-                  <a href={`/movie/${movie.id}`}>
-                    <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
-                  </a>
-                </div>
-              </TERipple>
-              <div className="p-6">
-                <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-                  {movie.title}
-                </h5>
-                <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                  {movie.overview.length > 150
-                    ? `${movie.overview.substring(0, 150)}...`
-                    : movie.overview}
-                </p>
+      {loading ? ( // Show spinner if loading is true
+        <div className="flex justify-center items-center h-full">
+          <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+        </div>
+      ) : (
+        <div className="flex flex-wrap -mx-4">
+          {error && <div>Error: {error}</div>}
 
+          {popularMovies.map((movie) => (
+            <div
+              key={movie.id}
+              className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-8"
+            >
+              <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
                 <TERipple>
-                  <a
-                    type="button"
-                    href={`/movie/${movie.id}`}
-                    className="inline-block rounded bg-info px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-danger-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-info-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-info-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                  >
-                    Details
-                  </a>
+                  <div className="relative overflow-hidden bg-cover bg-no-repeat">
+                    <img
+                      className="rounded-t-lg"
+                      src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                      alt={movie.title}
+                    />
+                    <button
+                      className="absolute top-4 right-4 z-50"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        favorites.some((favMovie) => favMovie.id === movie.id)
+                          ? handleRemoveFromFavorites(movie.id)
+                          : handleAddToFavorites(movie);
+                      }}
+                    >
+                      {favorites.some(
+                        (favMovie) => favMovie.id === movie.id
+                      ) ? (
+                        <FaHeart size={30} style={{ color: "red" }} />
+                      ) : (
+                        <CiHeart size={30} style={{ color: "white" }} />
+                      )}
+                    </button>
+                    <a href={`/movie/${movie.id}`}>
+                      <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+                    </a>
+                  </div>
                 </TERipple>
+                <div className="p-6">
+                  <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                    {movie.title}
+                  </h5>
+                  <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                    {movie.overview.length > 150
+                      ? `${movie.overview.substring(0, 150)}...`
+                      : movie.overview}
+                  </p>
+
+                  <TERipple>
+                    <a
+                      type="button"
+                      href={`/movie/${movie.id}`}
+                      className="inline-block rounded bg-info px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-danger-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-info-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-info-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                    >
+                      Details
+                    </a>
+                  </TERipple>
+                </div>
               </div>
             </div>
+          ))}
+          <div className="flex justify-center py-10 w-full">
+            <button
+              className="inline-block rounded bg-success px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#14a44d] transition duration-150 ease-in-out hover:bg-success-600 hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:bg-success-600 focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:outline-none focus:ring-0 active:bg-success-700 active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(20,164,77,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)]"
+              onClick={handlePrevPage}
+              disabled={currentPage === 1}
+            >
+              Previous
+            </button>
+            <span>
+              {currentPage} / {totalPages}
+            </span>
+            <button
+              className="inline-block rounded bg-success px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#14a44d] transition duration-150 ease-in-out hover:bg-success-600 hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:bg-success-600 focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:outline-none focus:ring-0 active:bg-success-700 active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(20,164,77,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)]"
+              onClick={handleNextPage}
+              disabled={currentPage === totalPages}
+            >
+              Next
+            </button>
           </div>
-        ))}
-        <div className="flex justify-center py-10 w-full">
-          <button
-            className="inline-block rounded bg-success px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#14a44d] transition duration-150 ease-in-out hover:bg-success-600 hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:bg-success-600 focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:outline-none focus:ring-0 active:bg-success-700 active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(20,164,77,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)]"
-            onClick={handlePrevPage}
-            disabled={currentPage === 1}
-          >
-            Previous
-          </button>
-          <span>
-            {currentPage} / {totalPages}
-          </span>
-          <button
-            className="inline-block rounded bg-success px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#14a44d] transition duration-150 ease-in-out hover:bg-success-600 hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:bg-success-600 focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:outline-none focus:ring-0 active:bg-success-700 active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(20,164,77,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)]"
-            onClick={handleNextPage}
-            disabled={currentPage === totalPages}
-          >
-            Next
-          </button>
         </div>
-      </div>
+      )}
     </div>
   );
 };
